6a45baff3b163a006a2d3f860c0babdafeda6cd7
seungho <75677520+seungho1206@users.noreply.github.com>
2022-12-23 19:13:46 +0900

    [Bronze II] Title: 대표값2, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        int tmp, sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 1; j < arr.length; j++){
+                if(arr[j-1] > arr[j]){
+                    tmp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = tmp;
+                }
+            }
+        }
+        int avg = sum / arr.length;
+        int median = arr[arr.length/2];
+        System.out.println(avg);
+        System.out.println(median);
+    }
+}
