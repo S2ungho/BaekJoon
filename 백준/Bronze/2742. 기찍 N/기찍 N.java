3403dabe73b4392b61b0449ecc1f8de1c2b3f105
seungho <75677520+seungho1206@users.noreply.github.com>
2023-01-01 23:36:46 +0900

    [Bronze IV] Title: 기찍 N, Time: 1096 ms, Memory: 32528 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = N; 0 < i; i--){
+            System.out.println(i);
+        }
+    }
+}
